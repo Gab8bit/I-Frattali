5f2833c37c0ba8856715024a57e3a12926ffb35d
Gab8bit <82293320+Gab8bit@users.noreply.github.com>
2022-08-03 13:55:34 +0200

    first upload

---
+++ js_mate.js
@@ -0,0 +1,3 @@
+function crediti(){
+    window.confirm("Realizzato da: \n\n- Nannini Gabriele (Struttura e impaginazione sito web e Homepage)\n- Conti Lorenzo (Mandelbrot, Curva di Koch, Triangolo di Sierpinski e Dimensione Frattale)\n- Reginelli Valerio (Frattali in Natura) \n- Pala Alessandro (Frattali Digitali)")
+}
